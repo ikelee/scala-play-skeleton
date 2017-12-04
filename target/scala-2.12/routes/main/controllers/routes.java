4364f562a0944429dd7b11dd1107abe904c41531
ike.lee <ike_0102@hotmail.com>
2017-12-03 20:11:56 -0800

    Adding necessary files

---
+++ target/scala-2.12/routes/main/controllers/routes.java
@@ -0,0 +1,21 @@
+
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/Users/ike.lee/Documents/scala-play-skeleton/conf/routes
+// @DATE:Sun Dec 03 20:06:50 PST 2017
+
+package controllers;
+
+import router.RoutesPrefix;
+
+public class routes {
+  
+  public static final controllers.ReverseAsyncController AsyncController = new controllers.ReverseAsyncController(RoutesPrefix.byNamePrefix());
+  public static final controllers.ReverseHomeController HomeController = new controllers.ReverseHomeController(RoutesPrefix.byNamePrefix());
+
+  public static class javascript {
+    
+    public static final controllers.javascript.ReverseAsyncController AsyncController = new controllers.javascript.ReverseAsyncController(RoutesPrefix.byNamePrefix());
+    public static final controllers.javascript.ReverseHomeController HomeController = new controllers.javascript.ReverseHomeController(RoutesPrefix.byNamePrefix());
+  }
+
+}
